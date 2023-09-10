295b4bdc2611d41882ae70e0db86637635a8228a
CEODemitri <demichole@gmail.com>
2023-09-10 03:03:12 -0500

    adding animations, first attempt

--- pages/script.js
+++ pages/script.js
@@ -1,27 +1,8 @@
-// const items = document.querySelectorAll('.shoe')
+const items = document.querySelectorAll('.shoe');
 
-// items.forEach((item) => {
-//     gsap.to(".shoe", {
-//         scrollTrigger: {
-//             trigger: ".container",
-//             markers: {
-//                 startColor: "#000",
-//                 endColor: "orange",
-//                 fontSize: "16px",
-//                 fontWeight: "bold",
-//                 indent: 20
-//             },
-//             start: "top top",
-//             end: "bottom +=400"
-//         },
-//         stagger: {amount: 0.1},
-//         scale: 0, 
-//         duration: 4
-//     })
-
-//     // Replace this to make the shoes animate one by one
-
-// });
+items.forEach((item) => {
+  gsap.to(item, { duration: 1, x: 100 });
+});
 
 // method --target -- variable
 gsap.to(".test", { 
