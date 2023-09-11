64f7c3bf76ee1a508075fb7184dfde01bea955c5
Color Codex <colorcodex8@Colors-iMac.local>
2023-09-10 22:43:50 -0500

    polishes the design a bit more by adding more icons and editing dark mode

--- pages/script.js
+++ pages/script.js
@@ -14,182 +14,183 @@
 //     toggleActions: 'play reverse play reverse',
 //   }
 // })
-gsap.set('.shoe1', {
+gsap.set(".shoe1", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe1', {
+  scale: 1,
+});
+gsap.from(".shoe1", {
   scrollTrigger: {
-    trigger: '.a',
-    start: 'top top',
-    end: '+=40',
-    scrub: 1
+    trigger: ".a",
+    start: "top top",
+    end: "+=40",
+    scrub: 1,
   },
   x: 300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe2', {
+gsap.set(".shoe2", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe2', {
+  scale: 1,
+});
+gsap.from(".shoe2", {
   scrollTrigger: {
-    trigger: '.b',
-    start: 'top 50%',
-    end: '+=50',
-    scrub: 1
+    trigger: ".b",
+    start: "top 20%",
+    end: "+=50",
+    scrub: 1,
   },
   x: -300,
   duration: 4,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe3', {
+gsap.set(".shoe3", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe3', {
+  scale: 1,
+});
+gsap.from(".shoe3", {
   scrollTrigger: {
-    trigger: '.c',
-    start: 'top 40%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".c",
+    start: "top 40%",
+    end: "+=40",
+    scrub: 1,
   },
   x: 300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe4', {
+gsap.set(".shoe4", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe4', {
+  scale: 1,
+});
+gsap.from(".shoe4", {
   scrollTrigger: {
-    trigger: '.d',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".d",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: -300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe5', {
+gsap.set(".shoe5", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe5', {
+  scale: 1,
+});
+gsap.from(".shoe5", {
   scrollTrigger: {
-    trigger: '.e',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".e",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: 300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe6', {
+gsap.set(".shoe6", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe6', {
+  scale: 1,
+});
+gsap.from(".shoe6", {
   scrollTrigger: {
-    trigger: '.f',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".f",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: -300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe7', {
+gsap.set(".shoe7", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe7', {
+  scale: 1,
+});
+gsap.from(".shoe7", {
   scrollTrigger: {
-    trigger: '.g',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".g",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: 300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe8', {
+gsap.set(".shoe8", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe8', {
+  scale: 1,
+});
+gsap.from(".shoe8", {
   scrollTrigger: {
-    trigger: '.h',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".h",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: -300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe9', {
+gsap.set(".shoe9", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe9', {
+  scale: 1,
+});
+gsap.from(".shoe9", {
   scrollTrigger: {
-    trigger: '.i',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".i",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: 300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
-gsap.set('.shoe10', {
+gsap.set(".shoe10", {
   opacity: 1,
-  scale: 1
-})
-gsap.from('.shoe10', {
+  scale: 1,
+});
+gsap.from(".shoe10", {
   scrollTrigger: {
-    trigger: '.j',
-    start: 'top 50%',
-    end: '+=40',
-    scrub: 1
+    trigger: ".j",
+    start: "top 50%",
+    end: "+=40",
+    scrub: 1,
   },
   x: -300,
   duration: 2,
   opacity: 0,
-  scale: 0.7
-})
+  scale: 0.7,
+});
 
 // method --target -- variable
-gsap.to(".test", { 
-    scale: 0.5,
-    duration: 3.5,
-    x: 250,
-    rotation: 360,
-    repeat: -2,
-    yoyo: true 
-})
+gsap.to(".test", {
+  scale: 0.5,
+  duration: 3.5,
+  x: 290,
+  rotation: 360,
+  repeat: -2,
+  yoyo: true,
+  fill: "rgb(255,255,255)",
+});
